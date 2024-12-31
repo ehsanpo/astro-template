69f6d0d5c7321e5c365989adaaeecca462f0a55e
Ehsan Pourhadi <ehsan.po@gmail.com>
2024-12-31 13:35:45 +0100

    added github build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://ehsanpo.github.io",
+  base: "astro-template",
   vite: {
     ssr: {
       noExternal: ["gsap"],
