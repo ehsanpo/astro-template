300c6d3b8bd95d9d3cc63194fa0b149cb8458745
Ehsan Pourhadi <ehsan.po@gmail.com>
2025-01-04 01:12:26 +0100

    contact page fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,70 +1,83 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
+  darkMode: ["class"],
   theme: {
-    extend: {
-      colors: {
-        primary2: {
-          50: "#f7ffe5",
-          100: "#ecffc7",
-          200: "#d9ff96",
-          300: "#befd59",
-          400: "#a2f427",
-          500: "#91f209",
-          600: "#63ae02",
-          700: "#4b8407",
-          800: "#3e680c",
-          900: "#34580f",
-          950: "#193102",
-        },
-        secondary2: {
-          50: "#f7f3ff",
-          100: "#efe9fe",
-          200: "#e2d6fe",
-          300: "#cbb5fd",
-          400: "#ad8bfa",
-          500: "#8b5cf6",
-          600: "#713aed",
-          700: "#5e28d9",
-          800: "#4e21b6",
-          900: "#421d95",
-          950: "#2a1065",
-        },
-        primary: {
-          50: "#fff9ec",
-          100: "#fff2d3",
-          200: "#ffe2a6",
-          300: "#ffcc6e",
-          400: "#ffaa33",
-          500: "#ff8f0c",
-          600: "#fd7702",
-          700: "#ca5704",
-          800: "#a0430c",
-          900: "#81390d",
-          950: "#461a04",
-        },
-        secondary: {
-          50: "#ffeff3",
-          100: "#ffe0e9",
-          200: "#ffc6d8",
-          300: "#ff97b8",
-          400: "#ff5d93",
-          500: "#ff2474",
-          600: "#ff0066",
-          700: "#d70056",
-          800: "#b40051",
-          900: "#99024b",
-          950: "#570024",
-        },
-      },
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-        basement: ["Basement", "sans-serif"],
-        inter: ["InterVariable", "sans-serif"],
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary2: {
+  				'50': '#f7ffe5',
+  				'100': '#ecffc7',
+  				'200': '#d9ff96',
+  				'300': '#befd59',
+  				'400': '#a2f427',
+  				'500': '#91f209',
+  				'600': '#63ae02',
+  				'700': '#4b8407',
+  				'800': '#3e680c',
+  				'900': '#34580f',
+  				'950': '#193102'
+  			},
+  			secondary2: {
+  				'50': '#f7f3ff',
+  				'100': '#efe9fe',
+  				'200': '#e2d6fe',
+  				'300': '#cbb5fd',
+  				'400': '#ad8bfa',
+  				'500': '#8b5cf6',
+  				'600': '#713aed',
+  				'700': '#5e28d9',
+  				'800': '#4e21b6',
+  				'900': '#421d95',
+  				'950': '#2a1065'
+  			},
+  			primary: {
+  				'50': '#fff9ec',
+  				'100': '#fff2d3',
+  				'200': '#ffe2a6',
+  				'300': '#ffcc6e',
+  				'400': '#ffaa33',
+  				'500': '#ff8f0c',
+  				'600': '#fd7702',
+  				'700': '#ca5704',
+  				'800': '#a0430c',
+  				'900': '#81390d',
+  				'950': '#461a04'
+  			},
+  			secondary: {
+  				'50': '#ffeff3',
+  				'100': '#ffe0e9',
+  				'200': '#ffc6d8',
+  				'300': '#ff97b8',
+  				'400': '#ff5d93',
+  				'500': '#ff2474',
+  				'600': '#ff0066',
+  				'700': '#d70056',
+  				'800': '#b40051',
+  				'900': '#99024b',
+  				'950': '#570024'
+  			}
+  		},
+  		fontFamily: {
+  			inter: [
+  				'InterVariable',
+  				'sans-serif'
+  			],
+  			poppins: [
+  				'Poppins',
+  				'sans-serif'
+  			],
+  			basement: [
+  				'Basement',
+  				'sans-serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
