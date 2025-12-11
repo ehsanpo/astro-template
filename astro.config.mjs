02a8a5cc0c163b1e8cca01e3c12369d5d27eb73e
Ehsan Pourhadi <ehsan.pourhadi@telavox.com>
2025-12-11 22:21:49 +0100

    feat: Add OptimizedImage component for improved image handling and lazy loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,16 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://ehsan-pourhadi.com/",
   base: "",
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+    remotePatterns: [
+      {
+        protocol: "https",
+      },
+    ],
+  },
   vite: {
     ssr: {
       noExternal: ["gsap"],
